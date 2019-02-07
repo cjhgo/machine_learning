e280d4b17ea5a4f5ae3177ba1c6bce8f54112fde
chen <1728533217en@gmail.com>
2019-02-07 22:26:08 +0800

    update

---
+++ svm/main.cpp
@@ -0,0 +1,22 @@
+#include<cmath>
+#include<tuple>
+#include<map>
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include<string>
+using namespace std;
+#include"read_file.h"
+#include"svm.cpp"
+int main(int argc, char const *argv[])
+{   
+    string fname="./uci/adult/adult.train";
+    vector<int> yv, yv_test;
+    vector< vector<int> > instances, instances_test;
+    read_data(fname, yv, instances);
+    read_data("./uci/adult/adult.test", yv_test, instances_test);
+    assert(yv.size() == instances.size());
+    SVM_SMO svm(instances, yv, instances.size());
+    return 0;
+}
